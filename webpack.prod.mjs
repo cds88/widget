f683655f822a46a5f3d364af9cd6635f15775af1
cds88 <sergiusz.cudo@wp.pl>
2024-10-05 18:07:06 +0200

    Fix build

--- webpack.prod.mjs
+++ webpack.prod.mjs
@@ -3,15 +3,25 @@ import { merge } from 'webpack-merge';
 import webpackCommon from './webpack.common.mjs';
 import TerserPlugin from 'terser-webpack-plugin';
 import webpackCommonProd from './webpack.common.build.mjs';
-import {__dirname} from './helpers.mjs'
-
- 
+import { __dirname } from './helpers.mjs';
 
 export default merge(webpackCommon, webpackCommonProd, {
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
   },
   optimization: {
     minimize: true,
